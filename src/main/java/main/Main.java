1ece08441d7638b1f9e0e1c47def320a853116de
Александр <hyperpositron@gmail.com>
2023-05-22 12:11:37 +0200

    Инициализация контекста Spring на основании созданниго класса конфигурации

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,11 @@
 package main;
 
+import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext();
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
         Parrot p = new Parrot();
-    } 
+    }
 }
