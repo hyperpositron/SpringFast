54537750f032e180a27dd15673535c0c3b1a912b
Александр <hyperpositron@gmail.com>
2023-05-22 12:09:00 +0200

    Создал ProjectConfig.java class , добавил сеттер и геттер. Шаг 1. Создаем класс конфигурации @Configuration Шаг 2. Создание метода, который возвращает бин, с аннотацией @Bean Шаг 3. Настройка Spring на инициализацию контекста с импользованием нового класса конфигурации.

--- src/main/java/main/Parrot.java
+++ src/main/java/main/Parrot.java
@@ -3,5 +3,12 @@ package main;
 public class Parrot {
     private String name;
 
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+
     //здесь должны быть сеттеры и геттеры
 }
