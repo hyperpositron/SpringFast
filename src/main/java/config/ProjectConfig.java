fa3798efa3f9adcfca81346aba08a249414ffc2d
Александр <hyperpositron@gmail.com>
2023-05-22 14:53:28 +0200

    Добавление в контекст Spring нескольких бинов(экземпляр класса) одного типа

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -6,18 +6,35 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ProjectConfig {
+
+    @Bean                /*Добавление в контекст Spring строки Hello*/
+    String hello() {
+        return "Hello";
+    }
+
+    @Bean               /*Добавление в контекст Spring целого числа 10*/
+    Integer ten() {
+        return 10;
+    }
+
     @Bean
-    Parrot parrot(){
+    Parrot parrot() {
         var p = new Parrot();
         p.setName("Koko");
         return p;
     }
-    @Bean                /*Добавление в контекст Spring строки Hello*/
-    String hello(){
-        return "Hello";
+
+    @Bean
+    Parrot parrot2() {
+        var p = new Parrot();
+        p.setName("Koko");
+        return p;
     }
-    @Bean               /*Добавление в контекст Spring целого числа 10*/
-    Integer ten(){
-        return 10;
+
+    @Bean
+    Parrot parrot3() {
+        var p = new Parrot();
+        p.setName("Miki");
+        return p;
     }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Parrot p = context.getBean(Parrot.class);
+        Parrot p = context.getBean("parrot1",Parrot.class);
         System.out.println(p.getName());
         String s = context.getBean(String.class);
         System.out.println(s);
